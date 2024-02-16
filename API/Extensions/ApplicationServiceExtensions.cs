801a4c9978ffb768c90bac294708150a8b77657f
Shuvankar Bhowmick <bhowmickshuvankar@gmail.com>
2024-02-16 19:35:00 +0530

    Added extension method for service injection in Program.cs

---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.Activities;
+using Application.Core;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(
+            this IServiceCollection services,
+            IConfiguration config
+        )
+        {
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+
+            // Data access
+            services.AddDbContext<DataContext>(opt =>
+            {
+                opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
+            });
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy(
+                    "CorsPolicy",
+                    policy =>
+                    {
+                        policy
+                            .AllowAnyHeader()
+                            .AllowAnyMethod()
+                            .WithOrigins("http://localhost:3000");
+                    }
+                );
+            });
+
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(List).Assembly));
+            services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+
+            return services;
+        }
+    }
+}
