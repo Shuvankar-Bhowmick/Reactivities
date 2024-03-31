886218d855d15639f95288e39a3955d00b17fde7
Shuvankar Bhowmick <bhowmickshuvankar@gmail.com>
2024-03-31 13:56:04 +0530

    Added formik for forms.

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -1,82 +1,82 @@
 // this file contains all of our requests that go to our API
-import { toast } from "react-toastify";
-import { Activity } from "../models/activity";
-import axios, { AxiosError, AxiosResponse } from "axios";
-import { router } from "../router/Routes";
-import { store } from "../stores/store";
+import { toast } from 'react-toastify';
+import { Activity } from '../models/activity';
+import axios, { AxiosError, AxiosResponse } from 'axios';
+import { router } from '../router/Routes';
+import { store } from '../stores/store';
 
 const sleep = (delay: number) => {
-  return new Promise(resolve => {
-    setTimeout(resolve, delay);
-  });
+	return new Promise((resolve) => {
+		setTimeout(resolve, delay);
+	});
 };
 
-axios.defaults.baseURL = "http://localhost:5000/api";
+axios.defaults.baseURL = 'http://localhost:5000/api';
 
 // axios interceptors (one of the most important features of axios).
 axios.interceptors.response.use(
-  async response => {
-    await sleep(1000);
-    return Promise.resolve(response);
-  },
-  (error: AxiosError) => {
-    const { data, status, config } = error.response as AxiosResponse;
+	async (response) => {
+		await sleep(1000);
+		return Promise.resolve(response);
+	},
+	(error: AxiosError) => {
+		const { data, status } = error.response as AxiosResponse;
 
-    switch (status) {
-      case 400:
-        if (config.method === "get" && "id" in data.errors) {
-          router.navigate("/not-found");
-        }
-        if (data.errors) {
-          const modalStateErrors = [];
+		switch (status) {
+			case 400:
+				if (data.errors !== undefined && 'id' in data.errors) {
+					router.navigate('/not-found');
+				}
+				if (data.errors) {
+					const modalStateErrors = [];
 
-          for (const key in data.errors) {
-            if (data.errors[key]) modalStateErrors.push(...data.errors[key]);
-          }
-          throw modalStateErrors;
-        } else toast.error(data);
-        break;
-      case 401:
-        toast.error("unauthorised");
-        break;
-      case 403:
-        toast.error("forbidden");
-        break;
-      case 404:
-        router.navigate("/not-found");
-        break;
-      case 500:
-        store.commonStore.setServerError(data);
-        router.navigate("/server-error");
-        break;
-    }
+					for (const key in data.errors) {
+						if (data.errors[key]) modalStateErrors.push(...data.errors[key]);
+					}
+					throw modalStateErrors;
+				} else toast.error(data);
+				break;
+			case 401:
+				toast.error('unauthorised');
+				break;
+			case 403:
+				toast.error('forbidden');
+				break;
+			case 404:
+				router.navigate('/not-found');
+				break;
+			case 500:
+				store.commonStore.setServerError(data);
+				router.navigate('/server-error');
+				break;
+		}
 
-    return Promise.reject(error);
-  }
+		return Promise.reject(error);
+	}
 );
 
 const responseBody = <T>(response: AxiosResponse<T>) => response.data;
 
 const requests = {
-  get: <T>(url: string) => axios.get<T>(url).then(responseBody),
-  post: <T>(url: string, body: object) =>
-    axios.post<T>(url, body).then(responseBody),
-  put: <T>(url: string, body: object) =>
-    axios.put<T>(url, body).then(responseBody),
-  del: <T>(url: string) => axios.delete<T>(url).then(responseBody),
+	get: <T>(url: string) => axios.get<T>(url).then(responseBody),
+	post: <T>(url: string, body: object) =>
+		axios.post<T>(url, body).then(responseBody),
+	put: <T>(url: string, body: object) =>
+		axios.put<T>(url, body).then(responseBody),
+	del: <T>(url: string) => axios.delete<T>(url).then(responseBody),
 };
 
 const Activites = {
-  list: () => requests.get<Activity[]>("/activities"),
-  details: (id: string) => requests.get<Activity>(`/activities/${id}`),
-  create: (activity: Activity) => requests.post<void>(`/activities`, activity),
-  update: (activity: Activity) =>
-    requests.put<void>(`/activities/${activity.id}`, activity),
-  delete: (id: string) => requests.del<void>(`activities/${id}`),
+	list: () => requests.get<Activity[]>('/activities'),
+	details: (id: string) => requests.get<Activity>(`/activities/${id}`),
+	create: (activity: Activity) => requests.post<void>(`/activities`, activity),
+	update: (activity: Activity) =>
+		requests.put<void>(`/activities/${activity.id}`, activity),
+	delete: (id: string) => requests.del<void>(`activities/${id}`),
 };
 
 const agent = {
-  Activites,
+	Activites,
 };
 
 export default agent;
--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,109 +1,117 @@
 import {
-  Button,
-  Form,
-  FormInput,
-  FormTextArea,
-  Segment,
-} from "semantic-ui-react";
-import { ChangeEvent, useEffect, useState } from "react";
-import { useStore } from "../../../app/stores/store";
-import { observer } from "mobx-react-lite";
-import LoadingComponent from "../../../app/layout/LoadingComponent";
-import { Link, useNavigate, useParams } from "react-router-dom";
-import { v4 as uuid } from "uuid";
+	Button,
+	Form,
+	FormInput,
+	FormTextArea,
+	Segment,
+} from 'semantic-ui-react';
+import { useEffect, useState } from 'react';
+import { useStore } from '../../../app/stores/store';
+import { observer } from 'mobx-react-lite';
+import LoadingComponent from '../../../app/layout/LoadingComponent';
+import { Link, useParams } from 'react-router-dom';
+import { Formik } from 'formik';
 
 export default observer(function ActivityForm() {
-  const { activityStore } = useStore();
-  const {
-    loading,
-    createActivity,
-    updateActivity,
-    loadActivity,
-    loadingInitial,
-  } = activityStore;
-  const { id } = useParams();
-  const navigate = useNavigate();
+	const { activityStore } = useStore();
+	const {
+		loading,
+		// createActivity,
+		// updateActivity,
+		loadActivity,
+		loadingInitial,
+	} = activityStore;
+	const { id } = useParams();
 
-  const INIT = {
-    id: "",
-    title: "",
-    category: "",
-    description: "",
-    date: "",
-    city: "",
-    venue: "",
-  };
-  const [activity, setActivity] = useState(INIT);
+	const INIT = {
+		id: '',
+		title: '',
+		category: '',
+		description: '',
+		date: '',
+		city: '',
+		venue: '',
+	};
+	const [activity, setActivity] = useState(INIT);
 
-  useEffect(() => {
-    if (id) loadActivity(id).then(activity => setActivity(activity!));
-  }, [id, loadActivity]);
+	useEffect(() => {
+		if (id) loadActivity(id).then((activity) => setActivity(activity!));
+	}, [id, loadActivity]);
 
-  if (loadingInitial) return <LoadingComponent />;
+	if (loadingInitial) return <LoadingComponent />;
 
-  function handleSubmit() {
-    if (!activity.id) {
-      activity.id = uuid();
-      createActivity(activity).then(() =>
-        navigate(`/activities/${activity.id}`)
-      );
-    } else updateActivity(activity).then(() => navigate(`/activities/${activity.id}`));
-  }
+	// function handleSubmit() {
+	//   if (!activity.id) {
+	//     activity.id = uuid();
+	//     createActivity(activity).then(() =>
+	//       navigate(`/activities/${activity.id}`)
+	//     );
+	//   } else updateActivity(activity).then(() => navigate(`/activities/${activity.id}`));
+	// }
 
-  function handleOnChange(
-    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  ) {
-    const { name, value } = e.target;
-    setActivity(prev => ({ ...prev, [name]: value }));
-  }
+	// function handleChange(
+	//   e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+	// ) {
+	//   const { name, value } = e.target;
+	//   setActivity(prev => ({ ...prev, [name]: value }));
+	// }
 
-  return (
-    <Segment clearing>
-      <Form onSubmit={handleSubmit} autoComplete="off">
-        <FormInput
-          name="title"
-          placeholder="Title"
-          value={activity.title}
-          onChange={handleOnChange}
-        ></FormInput>
-        <FormTextArea
-          placeholder="Description "
-          name="description"
-          value={activity.description}
-          onChange={handleOnChange}
-        ></FormTextArea>
-        <FormInput
-          placeholder="Category"
-          name="category"
-          value={activity.category}
-          onChange={handleOnChange}
-        ></FormInput>
-        <FormInput
-          name="date"
-          type="date"
-          placeholder="Date"
-          value={activity.date}
-          onChange={handleOnChange}
-        ></FormInput>
-        <FormInput
-          name="city"
-          placeholder="City"
-          value={activity.city}
-          onChange={handleOnChange}
-        ></FormInput>
-        <FormInput
-          name="venue"
-          placeholder="Venue"
-          value={activity.venue}
-          onChange={handleOnChange}
-        ></FormInput>
-        <Button loading={loading} floated="right" positive type="submit">
-          Submit
-        </Button>
-        <Button as={Link} to="/activities" floated="right" type="button">
-          Cancel
-        </Button>
-      </Form>
-    </Segment>
-  );
+	return (
+		<Segment clearing>
+			{/* to update the form values when the initialValues get updated we need to use the enableReinitialize prop here. */}
+			<Formik
+				enableReinitialize
+				initialValues={activity}
+				onSubmit={(values) => console.log(values)}
+			>
+				{({ values: activity, handleChange, handleSubmit }) => (
+					<Form onSubmit={handleSubmit} autoComplete="off">
+						<FormInput
+							name="title"
+							placeholder="Title"
+							value={activity.title}
+							onChange={handleChange}
+						></FormInput>
+						<FormTextArea
+							placeholder="Description "
+							name="description"
+							value={activity.description}
+							onChange={handleChange}
+						></FormTextArea>
+						<FormInput
+							placeholder="Category"
+							name="category"
+							value={activity.category}
+							onChange={handleChange}
+						></FormInput>
+						<FormInput
+							name="date"
+							type="date"
+							placeholder="Date"
+							value={activity.date}
+							onChange={handleChange}
+						></FormInput>
+						<FormInput
+							name="city"
+							placeholder="City"
+							value={activity.city}
+							onChange={handleChange}
+						></FormInput>
+						<FormInput
+							name="venue"
+							placeholder="Venue"
+							value={activity.venue}
+							onChange={handleChange}
+						></FormInput>
+						<Button loading={loading} floated="right" positive type="submit">
+							Submit
+						</Button>
+						<Button as={Link} to="/activities" floated="right" type="button">
+							Cancel
+						</Button>
+					</Form>
+				)}
+			</Formik>
+		</Segment>
+	);
 });
