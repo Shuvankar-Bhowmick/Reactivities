cac27301963c0e2726d75674d56e0c1c984d462f
Shuvankar Bhowmick <bhowmickshuvankar@gmail.com>
2024-04-07 19:03:09 +0530

    Validation using Formik

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,16 +1,11 @@
-import {
-	Button,
-	Form,
-	FormInput,
-	FormTextArea,
-	Segment,
-} from 'semantic-ui-react';
+import { Button, FormField, Label, Segment } from 'semantic-ui-react';
 import { useEffect, useState } from 'react';
 import { useStore } from '../../../app/stores/store';
 import { observer } from 'mobx-react-lite';
 import LoadingComponent from '../../../app/layout/LoadingComponent';
 import { Link, useParams } from 'react-router-dom';
-import { Formik } from 'formik';
+import { Form, Formik, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 
 export default observer(function ActivityForm() {
 	const { activityStore } = useStore();
@@ -33,6 +28,10 @@ export default observer(function ActivityForm() {
 		venue: '',
 	};
 	const [activity, setActivity] = useState(INIT);
+
+	const validationSchema = Yup.object({
+		title: Yup.string().required('The activity title is required'),
+	});
 
 	useEffect(() => {
 		if (id) loadActivity(id).then((activity) => setActivity(activity!));
@@ -60,49 +59,25 @@ export default observer(function ActivityForm() {
 		<Segment clearing>
 			{/* to update the form values when the initialValues get updated we need to use the enableReinitialize prop here. */}
 			<Formik
+				validationSchema={validationSchema}
 				enableReinitialize
 				initialValues={activity}
 				onSubmit={(values) => console.log(values)}
 			>
-				{({ values: activity, handleChange, handleSubmit }) => (
-					<Form onSubmit={handleSubmit} autoComplete="off">
-						<FormInput
-							name="title"
-							placeholder="Title"
-							value={activity.title}
-							onChange={handleChange}
-						></FormInput>
-						<FormTextArea
-							placeholder="Description "
-							name="description"
-							value={activity.description}
-							onChange={handleChange}
-						></FormTextArea>
-						<FormInput
-							placeholder="Category"
-							name="category"
-							value={activity.category}
-							onChange={handleChange}
-						></FormInput>
-						<FormInput
-							name="date"
-							type="date"
-							placeholder="Date"
-							value={activity.date}
-							onChange={handleChange}
-						></FormInput>
-						<FormInput
-							name="city"
-							placeholder="City"
-							value={activity.city}
-							onChange={handleChange}
-						></FormInput>
-						<FormInput
-							name="venue"
-							placeholder="Venue"
-							value={activity.venue}
-							onChange={handleChange}
-						></FormInput>
+				{({ handleSubmit }) => (
+					<Form className="ui form" onSubmit={handleSubmit} autoComplete="off">
+						<FormField>
+							<Field name="title" placeholder="Title" />
+							<ErrorMessage
+								name="title"
+								render={(err) => <Label basic color="red" content={err} />}
+							/>
+						</FormField>
+						<Field placeholder="Description " name="description" />
+						<Field placeholder="Category" name="category" />
+						<Field name="date" type="date" placeholder="Date" />
+						<Field name="city" placeholder="City" />
+						<Field name="venue" placeholder="Venue" />
 						<Button loading={loading} floated="right" positive type="submit">
 							Submit
 						</Button>
