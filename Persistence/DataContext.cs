9bcf3bdc0a4e7571bccd205c338122b19ac3dd55
shuvankar-bhowmick <bhowmickshuvankar@gmail.com>
2024-05-11 09:15:31 +0530

    added Identity user

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -1,13 +1,14 @@
 using Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Persistence
 {
-    public class DataContext : DbContext
+    public class DataContext : IdentityDbContext<AppUser>
     {
-        public DataContext(DbContextOptions options) : base(options)
-        {
-        }
+        // With the IdentityDbContext in place we don't need to create a new DbSet for our Identities as a lot of it will be taken care of by the IdentityDbContext class itself.
+        public DataContext(DbContextOptions options)
+            : base(options) { }
 
         // Activities represent the table name that gets created in the database.
         public DbSet<Activity> Activities { get; set; }
