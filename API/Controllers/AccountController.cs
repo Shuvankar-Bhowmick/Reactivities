658d5274da2f1cda6c4a6943a9a8ef0e92c3ef76
Shuvankar Bhowmick <bhowmickshuvankar@gmail.com>
2024-05-21 18:25:33 +0530

    Added new Dtos

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,11 @@
+/* The reason we don't use mediator */
+
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AccountController : ControllerBase { }
+}
--- API/DTOs/UserDto.cs
+++ API/DTOs/UserDto.cs
@@ -10,6 +10,6 @@ namespace API.DTOs
         public string DisplayName { get; set; }
         public string Token { get; set; }
         public string Image { get; set; }
-        public string UserName { get; set; }
+        public string Username { get; set; }
     }
 }
