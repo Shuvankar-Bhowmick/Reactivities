317305b3e1927e8667626f25025d2844c9e5e7ff
shuvankar-bhowmick <bhowmickshuvankar@gmail.com>
2024-07-11 19:20:13 +0530

    Add validation in RegisterDto

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -1,10 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.DTOs
 {
     public class RegisterDto
     {
+        /* Using data annotations for validations */
+
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Required]
+        [RegularExpression(
+            "(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{4, 8}$",
+            ErrorMessage = "Password must be complex"
+        )]
         public string Password { get; set; }
+
+        [Required]
         public string DisplayName { get; set; }
+
+        [Required]
         public string Username { get; set; }
     }
 }
